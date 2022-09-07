d14085dc8537cccb06861d482d3215634d4cc3a6
Martin Sandung <Martin Sandung@SANDUNG>
2022-09-07 12:30:34 +0200

    Add project files.

---
+++ Beräkna befruktning/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Beräkna_befruktning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Befruktningsdag();
+        }
+
+        static public void Befruktningsdag()
+        {
+            var martin = new DateTime(1987, 11, 26);
+            Console.WriteLine($"Född       : {martin.ToString("dd MMM yyyy")}"); 
+            var befruktning = martin.AddMonths(-9);
+            Console.WriteLine($"Befruktades: {befruktning.ToString("dd MMM yyyy")}");
+        }
+    }
+}
